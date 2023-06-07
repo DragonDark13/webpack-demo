f1b572cc0c6d7d9dc16279138998496522ee8982
Aleksandr Zachkevich <aleksdark13@gmail.com>
2023-06-07 15:13:47 +0300

    development

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
         print: './src/print.js',
     },
     devtool: 'inline-source-map',
+    devServer: {
+        static: './dist'
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Development',
@@ -18,5 +21,8 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
 };
